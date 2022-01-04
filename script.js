a9f4f972ccace5668832b49c85690d1ab9647c72
Maciek <msitkowski94@gmail.com>
2022-01-04 21:12:12 +0100

    run content_script to insert a container with notes input when page loads

---
+++ script.js
@@ -0,0 +1,30 @@
+main();
+
+async function main() {
+  const content = await getContent();
+  const secondary = content.querySelector("#secondary");
+
+  secondary.innerHTML = `
+    <div class="yt-notes">
+      <h2>Notes</h2>
+      <textarea id="notes"></textarea>
+     </div>
+  `;
+}
+
+/**
+ * When YouTube loads, the div with content is not immediately available.
+ * Try to get it every 500ms until it is available.
+ */
+function getContent() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const content = document.querySelector("#columns");
+      if (!content) {
+        resolve(getContent());
+      } else {
+        resolve(content);
+      }
+    }, 500);
+  });
+}
