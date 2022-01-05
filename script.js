7af574ab4c8d250de1b31d3696007ce152210558
Maciek <msitkowski94@gmail.com>
2022-01-05 20:43:50 +0100

    clean up the logic, add debug logs

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const DEBUG = false;
+
+console.log = function (...args) {
+  if (DEBUG) {
+    console.log(...args);
+  }
+};
+
 main();
 
 async function main() {
@@ -11,51 +19,68 @@ async function main() {
      </div>
   `;
 
+  const textarea = secondary.querySelector("#notes");
+
+  // Try to get old notes from the storage, and initialize the textarea with it
   const videoId = getVideoId();
   const oldNotes = await getNotes(videoId);
-  console.log("Do we have some old notes?");
-  console.log({ oldNotes });
   if (oldNotes) {
-    document.querySelector("#notes").value = oldNotes;
+    console.log("✅ Old notes found, setting textarea value");
+    textarea.value = oldNotes;
+  } else {
+    console.log("❌ No old notes found");
   }
 
   const saveBtn = secondary.querySelector("#save");
 
   saveBtn.addEventListener("click", () => {
-    console.log("Clicked save button");
-    const notes = secondary.querySelector("#notes").value;
-    console.log("Notes from textarea:");
-    console.log({ notes });
-    saveNotes(notes);
+    const videoId = getVideoId();
+    const notes = textarea.value;
+
+    saveNotes(videoId, notes);
   });
 }
 
-// Get text from textarea
+/**
+ * Get notes from the storage by the video ID
+ */
 async function getNotes(videoId) {
   try {
     const result = await chrome.storage.sync.get([videoId]);
+
     return result[videoId];
-  } catch {
-    console.log("Probably no results");
-    return null;
+  } catch (error) {
+    if (/Error in invocation of storage\.get/.test(error.message)) {
+      console.log("🥱 There are no notes for this video...");
+      return null;
+    } else {
+      console.error("💣 Something went wrong when getting the notes!");
+      console.error(error);
+    }
   }
 }
 
+/**
+ * Save notes in the storage
+ */
+async function saveNotes(videoId, notes) {
+  try {
+    await chrome.storage.sync.set({ [videoId]: notes });
+    console.log("🥳 Notes saved!");
+  } catch (error) {
+    console.error("💣 Something went wrong when saving the notes!");
+    console.error(error);
+  }
+}
+
+/**
+ * Get YouTube video ID from URL
+ */
 function getVideoId() {
   const url = window.location.href;
   const videoId = url.split("=")[1];
 
   return videoId;
-}
-
-function saveNotes() {
-  const notes = getNotes();
-  const videoId = getVideoId();
-
-  console.log("Saving notes:", notes);
-  console.log("Video ID:", videoId);
-
-  chrome.storage.sync.set({ [videoId]: notes });
 }
 
 /**
