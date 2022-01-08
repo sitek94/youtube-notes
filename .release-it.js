05f301cc45768c2308708aa3b979fdaf8344093b
Maciek <msitkowski94@gmail.com>
2022-01-08 16:27:25 +0100

    add release script

---
+++ .release-it.js
@@ -0,0 +1,17 @@
+module.exports = {
+  hooks: {
+    "after:bump": "npm run zip",
+    "after:release":
+      "echo Successfully released ${name} v${version} to ${repo.repository}.",
+  },
+  github: {
+    release: true,
+    assets: ["dist/*.zip"],
+  },
+  git: {
+    requireCleanWorkingDir: false,
+  },
+  npm: {
+    publish: false,
+  },
+};
